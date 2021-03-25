7b63e809edf1d3a8255cd818721a1b208d0685b5
PratikGarai <pratikgarai0208@gmail.com>
2021-03-25 14:20:09 +0530

    head moves with keyboad

--- Snake/script.js
+++ Snake/script.js
@@ -12,8 +12,16 @@ const col3 = "#ed460e";      // food
 var Snake = {
     head_x : undefined,
     head_y : undefined,
-    body : undefined
+    body : undefined,
+    direction : 1,
 } 
+
+var direction_mapper = {
+    1 : [0, -1],        // up
+    2 : [1, 0],         // right
+    3 : [0, 1],         // down
+    4 : [-1, 0]         // left
+}
 
 const start = () => {
     canvas = document.getElementById("myCanvas");
@@ -25,8 +33,8 @@ const start = () => {
 const init = () => {
     canvasContext
         .clearRect(
-            10,
-            10,
+            0,
+            0,
             canvas.width,
             canvas.height
         );
@@ -34,8 +42,8 @@ const init = () => {
     canvasContext.fillStyle = "black";
     canvasContext
         .fillRect(
-            10,
-            10,
+            0,
+            0,
             canvas.width, 
             canvas.height
         );
@@ -48,11 +56,25 @@ const init = () => {
 }
 
 const update = () => {
+    Snake.head_x = Snake.head_x+direction_mapper[Snake.direction][0];
+    Snake.head_y = Snake.head_y+direction_mapper[Snake.direction][1];
+
+    if(Snake.head_x>=rows)
+        Snake.head_x = 0;
+    if(Snake.head_y>=cols)
+        Snake.head_y = 0;
+    if(Snake.head_x<0)
+        Snake.head_x = rows-1;
+    if(Snake.head_y<0)
+        Snake.head_y = cols-1;
+}
+
+const draw = () => {
     canvasContext.fillStyle = "black";
     canvasContext
         .fillRect(
-            10,
-            10,
+            0,
+            0,
             canvas.width, 
             canvas.height
         );
@@ -80,13 +102,33 @@ const update = () => {
     });
 }
 
-const draw = () => {
+const keyHandler = (e) => {
+    // W : 119
+    // S : 115
+    // A : 97
+    // D : 100
+    switch(e.keyCode)
+    {
+        case 119 :
+            Snake.direction = 1;
+            break;
+        case 115 :
+            Snake.direction = 3;
+            break;
+        case 97 :
+            Snake.direction = 4;
+            break;
+        case 100 :
+            Snake.direction = 2;
+            break;
+    }
 }
 
 const mainLoop = () => {
     update();
     draw();
-    window.setTimeout(mainLoop, 1000/60);
+    window.setTimeout(mainLoop, 100);
 }
 
 document.addEventListener('DOMContentLoaded', start);
+document.addEventListener("keypress", keyHandler, true);
